d627417cb3620990a084e6997edd2b90e61b7763
Selim Waly <selimwalyy@gmail.com>
2024-06-25 00:51:44 +0300

    Record maximum depth value

---
+++ src/core/main/java/com/voltra/shockwave/java/model/Depth.java
@@ -0,0 +1,10 @@
+package com.voltra.shockwave.java.model;
+
+public final class Depth {
+
+	public static final int MAX_PLY = 256;
+	public static final int MAX_DEPTH = 64;
+
+	private Depth() {
+	}
+}

91f08bf19baf8612af4bb6da7e6776b9e93affe5
Selim Waly <selimwalyy@gmail.com>
2024-06-25 00:52:51 +0300

    Classify castling types

---
+++ src/core/main/java/com/voltra/shockwave/java/model/CastlingType.java
@@ -0,0 +1,16 @@
+package com.voltra.shockwave.java.model;
+
+public final class CastlingType {
+
+	public static final int KINGSIDE = 0;
+	public static final int QUEENSIDE = 1;
+
+	public static final int NOCASTLINGTYPE = 2;
+
+	public static final int[] values = {
+		KINGSIDE, QUEENSIDE
+	};
+
+	private CastlingType() {
+	}
+}

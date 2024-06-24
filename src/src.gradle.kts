11c7e2d87abf549ed110dacba8116ac5d5fa1b02
Selim Waly <selimwalyy@gmail.com>
2024-06-25 01:00:06 +0300

    Write Gradle Task

---
+++ src/src.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+	application
+}
+
+dependencies {
+	implementation(libs.jcpi)
+	testImplementation(libs.junit)
+	testImplementation(libs.assertj)
+}
+
+java {
+	toolchain {
+		languageVersion.set(JavaLanguageVersion.of(17))
+	}
+}
+
+tasks.withType<JavaCompile> {
+	options.compilerArgs.addAll(listOf("-Xlint:all", "-Werror"))
+}
+
+tasks.test {
+	useJUnitPlatform()
+}
+
+application {
+	mainClass.set("com.voltra.shockwave.java.Main")
+	executableDir = ""
+}
+
+tasks.named<JavaExec>("run") {
+	standardInput = System.`in`
+}
+
+tasks.withType<Tar> {
+	enabled = false
+}
+
+distributions {
+	main {
+		contents {
+			from(rootProject.layout.projectDirectory.dir("src/core/dist"))
+			from(rootProject.layout.projectDirectory.file("README.md"))
+			from(rootProject.layout.projectDirectory.file("CHANGES.md"))
+			from(rootProject.layout.projectDirectory.file("LICENSE"))
+		}
+	}
+}

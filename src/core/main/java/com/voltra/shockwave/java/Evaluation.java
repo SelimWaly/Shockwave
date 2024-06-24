caff8bb4f0336e546aaabf7173a1c5b9b6806d74
Selim Waly <selimwalyy@gmail.com>
2024-06-25 00:49:05 +0300

    Add king's safety to evaluation

---
+++ src/core/main/java/com/voltra/shockwave/java/Evaluation.java
@@ -0,0 +1,111 @@
+package com.voltra.shockwave.java;
+
+import com.voltra.shockwave.java.model.Piece;
+import com.voltra.shockwave.java.model.PieceType;
+import com.voltra.shockwave.java.model.Square;
+
+import static com.voltra.shockwave.java.model.Color.opposite;
+import static com.voltra.shockwave.java.model.Square.bishopDirections;
+import static com.voltra.shockwave.java.model.Square.knightDirections;
+import static com.voltra.shockwave.java.model.Square.queenDirections;
+import static com.voltra.shockwave.java.model.Square.rookDirections;
+
+final class Evaluation {
+
+	static final int TEMPO = 1;
+
+	private static final int MATERIAL_WEIGHT = 100;
+	private static final int MOBILITY_WEIGHT = 80;
+	private static final int MAX_WEIGHT = 100;
+
+	/**
+	 * Evaluates the position.
+	 *
+	 * @param position the position.
+	 * @return the evaluation value in centipawns.
+	 */
+	int evaluate(Position position) {
+		// Initialize
+		int myColor = position.activeColor;
+		int oppositeColor = opposite(myColor);
+		int value = 0;
+
+		// Evaluate material
+		int materialScore = (evaluateMaterial(myColor, position) - evaluateMaterial(oppositeColor, position))
+			* MATERIAL_WEIGHT / MAX_WEIGHT;
+		value += materialScore;
+
+		// Evaluate mobility
+		int mobilityScore = (evaluateMobility(myColor, position) - evaluateMobility(oppositeColor, position))
+			* MOBILITY_WEIGHT / MAX_WEIGHT;
+		value += mobilityScore;
+
+		// Add Tempo
+		value += TEMPO;
+
+		return value;
+	}
+
+	private int evaluateMaterial(int color, Position position) {
+		int material = position.material[color];
+
+		// Add bonus for bishop pair
+		if (Bitboard.size(position.pieces[color][PieceType.BISHOP]) >= 2) {
+			material += 50;
+		}
+
+		return material;
+	}
+
+	private int evaluateMobility(int color, Position position) {
+		int knightMobility = 0;
+		for (long squares = position.pieces[color][PieceType.KNIGHT]; squares != 0; squares = Bitboard.remainder(squares)) {
+			int square = Bitboard.next(squares);
+			knightMobility += evaluateMobility(position, square, knightDirections);
+		}
+
+		int bishopMobility = 0;
+		for (long squares = position.pieces[color][PieceType.BISHOP]; squares != 0; squares = Bitboard.remainder(squares)) {
+			int square = Bitboard.next(squares);
+			bishopMobility += evaluateMobility(position, square, bishopDirections);
+		}
+
+		int rookMobility = 0;
+		for (long squares = position.pieces[color][PieceType.ROOK]; squares != 0; squares = Bitboard.remainder(squares)) {
+			int square = Bitboard.next(squares);
+			rookMobility += evaluateMobility(position, square, rookDirections);
+		}
+
+		int queenMobility = 0;
+		for (long squares = position.pieces[color][PieceType.QUEEN]; squares != 0; squares = Bitboard.remainder(squares)) {
+			int square = Bitboard.next(squares);
+			queenMobility += evaluateMobility(position, square, queenDirections);
+		}
+
+		return knightMobility * 4
+			+ bishopMobility * 5
+			+ rookMobility * 2
+			+ queenMobility;
+	}
+
+	private int evaluateMobility(Position position, int square, int[] directions) {
+		int mobility = 0;
+		boolean sliding = PieceType.isSliding(Piece.getType(position.board[square]));
+
+		for (int direction : directions) {
+			int targetSquare = square + direction;
+
+			while (Square.isValid(targetSquare)) {
+				mobility++;
+
+				if (sliding && position.board[targetSquare] == Piece.NOPIECE) {
+					targetSquare += direction;
+				} else {
+					break;
+				}
+			}
+		}
+
+		return mobility;
+	}
+}

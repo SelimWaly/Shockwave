b4a086ed403005ed91da4029a431bdd3bf31ed01
Selim Waly <selimwalyy@gmail.com>
2024-06-25 00:55:32 +0300

    Classify numbers as files

---
+++ src/core/main/java/com/voltra/shockwave/java/model/File.java
@@ -0,0 +1,22 @@
+package com.voltra.shockwave.java.model;
+
+public final class File {
+
+	public static final int a = 0;
+	public static final int b = 1;
+	public static final int c = 2;
+	public static final int d = 3;
+	public static final int e = 4;
+	public static final int f = 5;
+	public static final int g = 6;
+	public static final int h = 7;
+
+	public static final int NOFILE = 8;
+
+	public static final int[] values = {
+		a, b, c, d, e, f, g, h
+	};
+
+	private File() {
+	}
+}
